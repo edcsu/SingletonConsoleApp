ea6e74af57184d5c5ca88eec5b0f154debb49bed
Ssewannonda Keith <38857594+edcsu@users.noreply.github.com>
2024-08-27 18:46:20 +0300

    feat: create eager singleton class

---
+++ SingletonConsoleApp/Singleton.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SingletonConsoleApp
+{
+    public sealed class Singleton
+    {
+        private static Singleton Instance { get; } = new();
+
+        private Singleton() { }
+    }
+}
